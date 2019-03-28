a6d2e71642361cbbb842c12fd4bbdfc6662620ee
Clemence Goh <clemencegoh@users.noreply.github.com>
2019-03-28 13:43:02 +0800

    init cipher and initial tempalting

--- webapp.py
+++ webapp.py
@@ -1,24 +1,46 @@
-from flask import Flask, request, abort, send_file, send_from_directory
+from flask import Flask, request, abort, send_file, send_from_directory, render_template
 import os
 import mysql.connector
+import sqlite3
 
 app = Flask(__name__, static_url_path='/static')
+DATABASE_PATH = "./database/database.db"
 
 
-#mydb = mysql.connector.connect(
-#    host='localhost',
-#    user='clemence',
-#    passwd='clemence1',
-#)
+def connectDB():
+    return sqlite3.connect(DATABASE_PATH)
+
+
+def createTables():
+    conn = connectDB()
+    cur = conn.cursor()
+    cur.execute("")
+
+
+def createCipherText(key, text):
+    """
+    Placeholder for now, turns plaintext into ciphertext using key
+    :param key: key for encrypting the text
+    :param text: plaintext used
+    :return: Ciphertext
+    """
+    return "hello world!"
+
+
 
 @app.route('/', methods=['GET'])
-def base_root():
-    return ''
+def home():
+    return render_template('index.html', ciphertext=createCipherText("",""))
 
 
 @app.route('/addstuff/<stuff>', methods=['GET'])
 def addStuff_test(stuff):
     return str(stuff)
+
+
+@app.route('/database', methods=['POST'])
+def searchDB():
+    return ""
 
 
 if __name__=='__main__':
