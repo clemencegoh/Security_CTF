61085192163277856742797cb3a8da9e5cbffecc
Peng Hiang <33290649+penghiang@users.noreply.github.com>
2019-03-28 15:21:49 +0800

    vignere with decrypt

--- vignere.py
+++ vignere.py
@@ -5,8 +5,7 @@ def caesar(plaintext: bytes, key: int, mode: str="e") -> bytes:
     key = key%256
     ciphertext: bytes = b''
     for i in plaintext:
-        i = ord(i)
-        ciphertext += bytes([i + key if mode.lower() == "e" else i - key])
+        ciphertext += bytes([(i + key)%256 if mode.lower() == "e" else (i - key)%256])
     return ciphertext
 
 def shiftCipher(inp:str, k:int, mode:str="e") -> str:
@@ -25,7 +24,7 @@ def shiftCipher(inp:str, k:int, mode:str="e") -> str:
 
 # print(caesar(b"1ab2", 50))
 
-def vignere(plaintext: bytes, key: bytes) -> bytes:
+def vignere(plaintext: bytes, key: bytes, mode: str = "e") -> bytes:
     if (type(key) == str):
         key = key.encode()
     plaintext_list: List[List[bytes]] = []
@@ -38,7 +37,7 @@ def vignere(plaintext: bytes, key: bytes) -> bytes:
     
     # then we encrypt each sublist with caesar cipher
     for i, char in enumerate(key):
-        ciphertext_list.append(caesar(plaintext_list[i], char, "e"))
+        ciphertext_list.append(caesar(plaintext_list[i], char, mode))
 
     # finally, stitch it all back
     for j in range(len(ciphertext_list[0])):
@@ -49,6 +48,18 @@ def vignere(plaintext: bytes, key: bytes) -> bytes:
             ciphertext_list[i] = ciphertext_list[i][1:]
     return ciphertext
 
+x = vignere(b"\x01abcdefg ", b"\x06\xfe\x04\x03\x02")
 
-print(vignere("abcdefg ", b"\x02\x02\x02\x02\x02"))
+print(x)
+print(vignere(x, b"\x06\xfe\x04\x03\x02", "d"))
 
+
+# with open("week3/writeup.pdf", "rb") as f:
+#     x = f.readlines()
+#     x = b''.join(x)
+#     print(x)
+#     l = vignere(x, b"\x06\x05\x04\x03\x02\x04\x03\x02\x04\xff\x02")
+#     print(vignere(l, b"\x06\x05\x04\x03\x02\x04\x03\x02\x04\xff\x02", "d"))
+
+# a 0.5mb file takes 10s to encrypt
+# q. inefficient
